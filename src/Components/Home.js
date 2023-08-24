a1ecd83b48e40e3298b09136484558f21b955910
Ikhwan <khusayni@gmail.com>
2023-08-24 14:19:24 +0800

    add dropdown

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,8 @@ import { useFetch } from "../fetchHook";
 import Dropdown from "./Dropdown";
 const Home = () => {
     const { data, loading } = useFetch('https://jsonplaceholder.org/users');
-    console.log(data);
-    return (<>{loading ? <h3>Pending..</h3> : data.map(item => (<div key={item.id}><Dropdown />{item.firstname} {item.lastname}</div>))}
+    return (<>{loading ? <h3>Pending..</h3> :
+        <Dropdown data={data} />}
     </>);
 }
 export default Home;
