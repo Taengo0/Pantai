27716bfd01e64018f9e34ee494ab4514d3e958da
Ikhwan <khusayni@gmail.com>
2023-08-25 02:56:40 +0800

    add graph

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,38 +1,71 @@
 import { useFetch } from "../fetchHook";
-import Chart from 'chart.js/auto';
 import Dropdown from "./Dropdown";
+import {useState} from "react";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const Home = () => {
     const users= useFetch('https://jsonplaceholder.org/users');
     const { data, loading } = useFetch('https://api.frankfurter.app/2010-01-01..2010-01-31');
+
+    const [graphData, setGraphData] = useState({
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: [12, 43, 21, 34, 76, 76, 32],
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Dataset 2',
+                data: [43, 54, 21, 34, 76, 54, 32],
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    });
+    const [graphOptions, setGraphOptions] = useState({
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart',
+            },
+        },
+    });
     console.log(data);
 
-    // graph part
-    new Chart(document.getElementById('myChart'), {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
+    return (
+        <>
+            {users.loading ? <h3>Pending..</h3> :
+                <div>
+                    <Dropdown data={users.data} />
+                    {loading ? <h3>Loading graph data..</h3> : <Line options={graphOptions} data={graphData} />}
+                </div>
             }
-        }
-    });
-
-    return (<>{users.loading ? <h3>Pending..</h3> :
-        <div>
-            <Dropdown data={users.data} />
-            <div><canvas id="myChart"></canvas></div>
-        </div>
-    }
-    </>);
+        </>);
 }
 export default Home;
