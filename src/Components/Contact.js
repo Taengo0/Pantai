507962ba42d73ea48d5892ba3f947d03d7444240
ikhwan <khusayni@gmail.com>
2023-09-12 00:13:00 +0800

    update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,18 +1,20 @@
 import video from "../Asset/Siti Nurulkhadijah & Syed Ikhwan.MP4";
+
 const Contact = () => {
+  const imageArray = [
+    "https://drive.google.com/uc?export=view&id=1GoRrfAG16u24IdEGxdEEF0CkskhcoPF_",
+    "https://drive.google.com/uc?export=view&id=1lqk-bJZ_hBKLqC9RfWpya6M3natBN0cX",
+    "https://drive.google.com/uc?export=view&id=1aVFYE_pPuFjv7XHr7fDY6-yJZljE2aLk",
+  ];
   return (
     <>
-      <img
-        src="https://drive.google.com/uc?export=view&id=1GoRrfAG16u24IdEGxdEEF0CkskhcoPF_"
-        alt="wedding"
-        className="imageClass"
-      />
+      {imageArray.map((item) => (
+        <section>
+          <img src={item} alt="wedding" className="imageClass" />
+        </section>
+      ))}
       <div className="imageText">
         <div>Ikhwan and Nurul</div>
-      </div>
-      {/*<video src='https://drive.google.com/uc?export=view&id=1b1f9J12GBP5qmv42gsKT3KFp8KICzaeY' />*/}
-      <div className="videoContainer">
-        {/*<video src={video} autoPlay loop />*/}
       </div>
     </>
   );
