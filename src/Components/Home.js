eac7f3f7ca9eb7b06f2c2ead4fdeb9bb93d9528c
Ikhwan <ikhwan.azli@ihhhealthcare.com>
2024-04-25 17:11:49 +0800

    add graph back

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,18 @@
+import CurrencyGraph from "./GraphCurrency";
+
 const FormSection = () => {
   return (
     <form>
       <div className="formContainer">
-        <div>
-          <label>Name / Nama</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>No. of pax / Berapa orang datang</label>
-          <input type="number" />
-        </div>
+        {/*<div>*/}
+        {/*  <label>Name / Nama</label>*/}
+        {/*  <input type="text" />*/}
+        {/*</div>*/}
+        {/*<div>*/}
+        {/*  <label>No. of pax / Berapa orang datang</label>*/}
+        {/*  <input type="number" />*/}
+        {/*</div>*/}
+        <div><CurrencyGraph/></div>
       </div>
     </form>
   );
